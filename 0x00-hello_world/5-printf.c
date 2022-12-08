0213fe4dcba7f2192f48207c56616c7a55a54cad
Dera <chideronyekachi04@gmail.com>
2022-12-08 13:32:56 -0800

    edited 5-printf.c  file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>  
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -8,6 +8,7 @@
 
 int main(void)
 {
+	/* This function prints out text using the printf functio */
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
