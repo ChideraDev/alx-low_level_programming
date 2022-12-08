d243636bc67366c816cceab6db9220a08f0d21e1
Dera <chideronyekachi04@gmail.com>
2022-12-08 12:20:10 -0800

    edited 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 #include<stdio.h>
 int main(){
-	char myC = "Programming is like building a multilingual puzzle";
+	char myC = '"Programming is like building a multilingual puzzle';
        	puts(myC);
 
 
