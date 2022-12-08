5291ae63e7005f75bdde3cde4a11e90f1b1b8ff4
Dera <chideronyekachi04@gmail.com>
2022-12-08 12:56:26 -0800

    edited 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 /**                                                                                                                                                          * main - Entry point
 *
 * Return: Always 0 (Success)
-*/                                                                                                                                                       
+*/
+
 int main(void)                                                                                                                                               
 {                                                                                                                                                             printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                        
 		                                                                                                                                                             
