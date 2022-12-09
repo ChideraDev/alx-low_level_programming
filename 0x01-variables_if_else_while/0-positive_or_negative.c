8823f192c408bb78cfeccaf6596df9608bb18ed9
Dera <chideronyekachi04@gmail.com>
2022-12-09 10:00:09 -0800

    created 0-positive_or_negative.c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+
+	if(n > 0)
+	   printf("is positive\n");
+	else if(n == 0)
+	   printf("is zero\n");
+	else
+	   printf("is negative\n");
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+source code in order to print whether the number stored in the variable n is positive or negative
