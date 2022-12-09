c703d0d7e25ee734971180a4e2a229c02663a5e0
Dera <chideronyekachi04@gmail.com>
2022-12-09 13:42:24 -0800

    edited 3-print_alphabets.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int c;
 
-	for(c = 'a'; c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; ++c)
 	{
 		putchar(c);
 	}
-	for(c = 'A'; c <= 'Z' ; ++c)
+	for (c = 'A'; c <= 'Z' ; ++c)
 	{
 		putchar(c);
 	}
