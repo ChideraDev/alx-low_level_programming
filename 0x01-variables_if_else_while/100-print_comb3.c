94371e91997dac45e36ab2b799fbe1808e208403
Dera <chideronyekachi04@gmail.com>
2022-12-12 15:09:33 -0800

    add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int start = 1;
+	int end = 85;
+
+	for (a = start; a <= end; a++)
+	{
+		int tens = a / 10;
+		int ones = a % 10;
+
+		putchar('0' + tens);
+		putchar('0' + ones);
+
+		if (a < end)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
