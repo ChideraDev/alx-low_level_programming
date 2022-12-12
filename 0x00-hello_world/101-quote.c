513b0bce765bc42bada770e2d7c799fd8bd1aba4
Dera <chideronyekachi04@gmail.com>
2022-12-12 14:55:29 -0800

    edited file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -7,7 +8,10 @@
 
 int main(void)
 {
-	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf (stderr, "%s", c);
-	return (1)
+	char str1[] = "and that piece of art is useful\"";
+	char str2[] = " - Dora Korpar, 2015-10-19\n";
+
+	strcat(str1, str2);
+	fprintf (stderr, "%s", str1);
+	return (1);
 }
