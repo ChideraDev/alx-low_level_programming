72e8fe0f4c18a539fa3081cb20326e4bcbb14d09
Dera <chideronyekachi04@gmail.com>
2022-12-13 15:57:51 -0800

    created 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -prints add two numbers
+ * @numOne: prints int numOne
+ * @numTwo: prints int numTwo
+ * Return: Always 0.
+ */
+
+int add(int numOne, int numTwo)
+{
+	return (numOne + numTwo);
+}
