0d8c2adb674aaf72fe5e28ff37406104fddc1164
Dera <chideronyekachi04@gmail.com>
2022-12-13 13:31:58 -0800

    created 7-print_last_digit.c  file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,9 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
+	if (last_digit < 0)
+		last_digit *= -1;
 
-	_putchar(ld + '0');
+	_putchar(last_digit + '0');
 	return (0);
 }
