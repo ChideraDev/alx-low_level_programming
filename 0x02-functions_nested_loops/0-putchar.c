b28f37fad90a8395de8e5d2f4707443bc5751c66
Dera <chideronyekachi04@gmail.com>
2022-12-13 06:05:55 -0800

    update file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - Entry point
@@ -12,8 +12,8 @@ int main(void)
 
 	for (int i = 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
+		_putchar(str[i]);
 	}
-	putchar("\n");
+	_putchar("\n");
 	return (0);
 }
