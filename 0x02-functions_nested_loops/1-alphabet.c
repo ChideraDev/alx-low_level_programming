47658cecea5e48be4d708c6d050d8b0bdf0a4068
Dera <chideronyekachi04@gmail.com>
2022-12-13 08:56:26 -0800

    created 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char chr;
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		_putchar(chr);
+	}
+	_putchar('\n');
+}
+int main(void){
+	print_alphabet();
+	return (0);
+}
