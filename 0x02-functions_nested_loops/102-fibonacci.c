b2b74f9cc32e003c9cccc54611a6c38ed8144208
Dera <chideraonyekachi04@gmail.com>
2022-12-14 17:31:08 +0100

    created 102-fibonacci.c file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints first 50 Fibonacci numbers
- * with 1 and 2
- * 
- * Return: Always 0.
- */
+* main - Prints first 50 Fibonacci numbers, starting with 1 and 2
+*
+* Return: Always 0.
+*/
 
 int main(void)
 {
@@ -23,7 +22,8 @@ n2 = sum;
 if (count == 49)
 printf("\n");
 else
-printf(",");
-}
+printf(", "); }
+
 return (0);}
+
 
