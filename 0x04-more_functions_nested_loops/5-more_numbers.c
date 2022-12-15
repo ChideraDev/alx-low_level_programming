6a3bd983cf9940e9a3880a32131c5648a41954d1
Dera <chideronyekachi04@gmail.com>
2022-12-15 15:32:56 -0800

    created new files

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (j >= 10)
+			if (c >= 10)
 			_putchar('1');
-			_putchar(j % 10 + '0');
+			_putchar(c % 10 + '0');
 		}
 		_putchar('\n');
 	}
