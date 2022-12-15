464437521e69e7db17835a90c4bc359dec87109f
Dera <chideronyekachi04@gmail.com>
2022-12-15 14:31:31 -0800

    created 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void);
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' || a != '4')
+			_putchar(a);
+	}
+	_putchar('\n');
+}
