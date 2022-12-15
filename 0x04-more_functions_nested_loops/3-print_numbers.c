9b8bf6dc3593ca272a69405568cb9a861bb9c15c
Dera <chideronyekachi04@gmail.com>
2022-12-15 14:05:10 -0800

    created 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print num from  0 - 9.
+ * print_numbers - print num from  0 - 9
  * Return: void
  */
 
@@ -9,7 +9,7 @@ void print_numbers(void)
 {
 	char i;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
