ac654b5f114ec8a855c1906c9ca85811fde91940
Dera <chideronyekachi04@gmail.com>
2022-12-15 04:56:10 -0800

    created 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _mul - multiplies two integers
+ * and returns the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 function that checks for uppercase character
 function that checks for a digit (0 through 9)
+ function that multiplies two integers
