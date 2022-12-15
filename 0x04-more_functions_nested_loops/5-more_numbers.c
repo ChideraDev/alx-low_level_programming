6c1909fadc5bc4750d2a7e3d31316bc278fd4f55
Dera <chideronyekachi04@gmail.com>
2022-12-15 15:30:16 -0800

    created new files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
