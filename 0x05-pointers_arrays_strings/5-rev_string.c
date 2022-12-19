fc2a1d2b2bb5b22119ab467febfc0cf06d057fb4
Dera <chideraonyekachi04@gmail.com>
2022-12-19 23:59:05 +0100

    fix betty issue

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,10 +19,10 @@ void rev_string(char *s)
 		count++;
 	}
 
-	for (i = 0; i < count / 2; i++)  
-    {  
-        temp = s[i];  
-        s[i] = s[count - i - 1];  
-        s[count - i - 1] = temp;  
-    }  
+	for (i = 0; i < count / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[count - i - 1];
+		s[count - i - 1] = temp;
+	}
 }
