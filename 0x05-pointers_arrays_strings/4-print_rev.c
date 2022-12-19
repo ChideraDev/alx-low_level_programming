9ce57669a41c8ed4d8dbd543ae901c5b67ec4c89
Dera <chideraonyekachi04@gmail.com>
2022-12-19 23:27:59 +0100

    fix merge conflict

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: string to reverse
- *
- * Return: void
- */
+* print_rev - prints a string, in reverse, followed by a new line.
+* @s: the string to print
+*
+* Return: void
+*/
+
 void print_rev(char *s)
 {
-	int count = 0;
-	int j;
+int count = 0;
+int j;
+int i;
 
-	for (j = 0; s[j] != '\0'; j++)
-	{
-		count++;
-	}
+for (j = 0; s[j] != '\0'; j++)
+{
+count++;
+}
 
-	int usable_length = count - 1;
-	int i;
-
-	for (i = usable_length; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
+for (i = count -1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
