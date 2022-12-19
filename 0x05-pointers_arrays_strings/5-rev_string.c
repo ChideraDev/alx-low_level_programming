04c256fe4439a0f99657c886a891046f0ca52885
Dera <chideraonyekachi04@gmail.com>
2022-12-19 23:56:31 +0100

    add rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string stored in a pointer
+ * @s: the string to reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+	int j;
+	int i;
+	char temp = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		count++;
+	}
+
+	for (i = 0; i < count / 2; i++)  
+    {  
+        temp = s[i];  
+        s[i] = s[count - i - 1];  
+        s[count - i - 1] = temp;  
+    }  
+}
