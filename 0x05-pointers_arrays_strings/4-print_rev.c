2201b35d38a3ad2889e93be748fe6a7c6711060b
Dera <chideronyekachi04@gmail.com>
2022-12-19 13:59:31 -0800

    updated file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		count++;
+	}
+
+	int usable_length = count -1;
+	int i;
+
+	for (i = usable_length; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ function that takes a pointer to an int as parameter and updates the value it po
 function that swaps the values of two integers.
 function that returns the length of a string
 function that prints a string, followed by a new line, to stdout
+function that prints a string, in reverse, followed by a new line.
