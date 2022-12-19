1fb3f35f00aec8268fde0b9fd4d58f96660b312a
Dera <chideraonyekachi04@gmail.com>
2022-12-20 00:46:59 +0100

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string followed by a new line
+ * @str: the string to print every other character
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	for(i = 0; str[i] != '\0'; i = i + 2)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
