0ae436768ad6128c6d99c787e72f79b79f1bd92a
Dera <chideraonyekachi04@gmail.com>
2022-12-20 02:32:04 +0100

    updated new files

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,13 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a + i));
-		if (i != n - 1)
+		printf("%d", a[i]);
+		if (i != n - 1){
 		printf(", ");
+		}
 	}
+	printf("\n");
 }
