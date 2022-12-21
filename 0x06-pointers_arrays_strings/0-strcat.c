2cbb00776e5240be649c38c50b8b9a0f82f7074a
Dera <chideraonyekachi04@gmail.com>
2022-12-21 23:05:22 +0100

    add Readme file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcat - Append a string to the end of another string
+* @dest: The string appended to.
+* @src: The string to append
+*
+* Return: Always 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	while(*dest!='\0')
+	dest++;
+
+	while(*src!='\0')
+	{
+	*dest=*src;
+	src++;
+	dest++;
+	}
+	*dest='\0';
+}
