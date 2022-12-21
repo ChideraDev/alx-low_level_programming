4bc7eb01a534805756a1e7afce084f5e52a6b77f
Dera <chideronyekachi04@gmail.com>
2022-12-21 15:58:35 -0800

    corrected betty error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	while(*dest!='\0')
+	while (*dest != '\0')
 	dest++;
 
-	while(*src!='\0')
+	while (*src != '\0')
 	{
-	*dest=*src;
+	*dest = *src;
 	src++;
 	dest++;
 	}
-	*dest='\0';
+	*dest = '\0';
 	return (dest);
 }
