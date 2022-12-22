4262ff4721b33f04e5f4630eef745b7b14002ae7
Dera <chideraonyekachi04@gmail.com>
2022-12-22 18:18:58 +0100

    updated new files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,14 +9,13 @@
 
 char *string_toupper(char *n)
 {
-	int i;
-	i = 0;
-	while (n[i] != '\0')
+	int i = 0;
+	while (n[i])
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-		n[i] = n[i] - 32;
+		n[i] -= 32;
+
 		i++;
-		}
+	}
 		return (n);
-		}
-		
+}
