99211caf690e40d6bdf0b6b6a10a21bf33834459
Dera <chideraonyekachi04@gmail.com>
2023-01-02 21:14:56 +0100

    created new files

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* set_string: sets the value of a pointer to a char
+* set_string - sets the value of a pointer to a char
 * @s: pointer to pointer
 * @to: pointer to char
 */
