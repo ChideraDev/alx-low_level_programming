131011fec72c2b51d36dbc6f925be2a97f6a299f
Dera <chideraonyekachi04@gmail.com>
2023-01-04 18:36:46 +0100

    created new files

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* helperFunction - returns 0 or 1
+* @num: number being checked
+* @i: possible factor of the number
+* Return: 0 if not prime, 1 if prime
+*/
+int helperFunction(int num, int i)
+{
+	if (i < 0)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (helperFunction(num, i + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+* is_prime_number - checks if number is prime or not
+* @n: number to check
+* 
+* Return: 1 if number is prime
+* 0 if number is not prime
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperFunction(n, 2));
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,4 @@ function that prints a string in reverse.
 function that returns the length of a string.
  function that returns the factorial of a given number.
  function that returns the value of x raised to the power of y.
+ function that returns 1 if the input integer is a prime number, otherwise return 0.
