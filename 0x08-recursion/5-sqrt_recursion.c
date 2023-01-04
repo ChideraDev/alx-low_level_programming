5003b7c754eef9beac5a63b48271d8cfe6040023
Dera <chideraonyekachi04@gmail.com>
2023-01-04 15:24:34 +0100

    created new files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,7 @@ int helperFunction(int num, int pSqrt)
 		if ((pSqrt * pSqrt) > num)
 		return (-1);
 		else
-		return (helperFunction (num, pSqrt + 1));
+		return (helperFunction(num, pSqrt + 1));
 	}
 }
 /**
@@ -32,5 +32,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 	return (-1);
 	else
-	return (helperFunction(n,0));
+	return (helperFunction(n, 0));
 }
