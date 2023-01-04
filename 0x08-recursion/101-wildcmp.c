ace0fa22446c48bc51952b4218e9265e04792138
Dera <chideraonyekachi04@gmail.com>
2023-01-04 22:12:11 +0100

    created new files

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,9 +14,9 @@ int wildcmp(char *str_one, char *str_two)
 	if (*str_one == *str_two)
 	return (wildcmp(str_one + 1, str_two + 1));
 	
-	if (str_two == '')
+	if (*str_two == '*')
 	{
-		if (str_two == '' && *(str_two + 1) != '\0' && *str_one == '\0')
+		if (*str_two == '*' && *(str_two + 1) != '\0' && *str_one == '\0')
 		return (0);
 		if (wildcmp(str_one, str_two + 1) || wildcmp(str_one + 1, str_two))
 		return (1);
