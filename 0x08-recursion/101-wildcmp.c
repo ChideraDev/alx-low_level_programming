b9f0c4c39e8ab6f24f41ccbfe70c33c3d4473781
Dera <chideraonyekachi04@gmail.com>
2023-01-04 22:01:37 +0100

    created new files

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * wildcmp - function to compares two strings which
- * could be identical when comparing wildcards.
- * @str_one: the plain string to compare
- * @str_two: the string with wild character
- * Return: 1 on success and -1 on error.
- */
+* wildcmp - function to compares two strings which
+* could be identical when comparing wildcards.
+* @str_one: the plain string to compare
+* @str_two: the string with wild character
+* Return: 1 on success and -1 on error.
+*/
 int wildcmp(char *str_one, char *str_two)
 {
 	if (*str_one == '\0' && *str_two == '\0')
