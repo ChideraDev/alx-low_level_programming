6ba200894d5a29df155d5e184e52101b23a681a6
Dera <chideraonyekachi04@gmail.com>
2023-01-04 14:33:31 +0100

    created new files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
 */
 int _strlen_recursion(char *s)
 {
-    if (*s)
-    {
-        return(1 + _strlen_recursion(s + 1));
-    }
-    else
-    {
-        return (0);
-    }
+	if (*s)
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
 }
