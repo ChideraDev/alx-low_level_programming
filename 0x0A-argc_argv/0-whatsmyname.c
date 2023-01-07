0bfd2ee804c332adf613e6ea7c8199041fce0e22
Dera <chideraonyekachi04@gmail.com>
2023-01-07 15:26:56 +0100

    created new files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Print the name of the program
+* @argc - Count the number of arguments
+* @argv - Arguments
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
