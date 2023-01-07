95fe1b1b448414a1b030c58e52c4e76524af2e8a
Dera <chideraonyekachi04@gmail.com>
2023-01-07 16:00:41 +0100

    created new files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
 * main - Print the name of the program
-* @argc - Count arguments
-* @argv - Arguments
+* @argc : Count arguments
+* @argv : Arguments
 *
 * Return: Always 0 (success)
 */
